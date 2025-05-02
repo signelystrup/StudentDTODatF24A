3d86cbed3021df8ceb30a3d556c2d681316c5050
signelystrup <166133407+signelystrup@users.noreply.github.com>
2025-05-02 09:55:06 +0200

    tilføjet h2 dependency og controller test klasse

---
+++ src/test/java/dk/kea/studentdtodatf24a/api/StudentControllerTest.java
@@ -0,0 +1,39 @@
+package dk.kea.studentdtodatf24a.api;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentControllerTest {
+/* delete?
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+*/
+
+    @Test
+    void getAllStudents() {
+    }
+
+    @Test
+    void getStudentById() {
+    }
+
+    @Test
+    void createStudent() {
+    }
+
+    @Test
+    void updateStudent() {
+    }
+
+    @Test
+    void deleteStudent() {
+    }
+}
